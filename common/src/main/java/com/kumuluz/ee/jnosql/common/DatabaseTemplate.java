a7511cfbe80e63804411f5ecde45237fad095f4d
Klemen Kobau <klemen.kobau@gmail.com>
2019-07-30 10:44:55 +0200

    including artemis core dependency

---
+++ common/src/main/java/com/kumuluz/ee/jnosql/common/DatabaseTemplate.java
@@ -0,0 +1,116 @@
+package com.kumuluz.ee.jnosql.common;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Inject;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Dependent
+public class DatabaseTemplate {
+
+	private static final Logger log = Logger.getLogger(DatabaseTemplate.class.getName());
+
+	private static final String graphTemplateClassName = "org.jnosql.artemis.graph.GraphTemplate";
+	private static final String columnTemplateClassName = "org.jnosql.artemis.column.ColumnTemplate";
+	private static final String documentTemplateClassName = "org.jnosql.artemis.document.DocumentTemplate";
+	private static final String keyValueTemplateClassName = "org.jnosql.artemis.key.KeyValueTemplate";
+
+	private Bean<?> databaseTemplate;
+	private Class<?> databaseClass;
+	private DatabaseType databaseType;
+
+	@Inject
+	public DatabaseTemplate(InjectionPoint ip, BeanManager bm) {
+
+		DatabaseLogic databaseAnnotation = null;
+
+		for (Annotation annotation : ip.getAnnotated().getAnnotations()) {
+			if (annotation.annotationType().isAssignableFrom(DatabaseLogic.class)) {
+				databaseAnnotation = ((DatabaseLogic) annotation);
+				break;
+			}
+		}
+
+		if (databaseAnnotation == null) {
+			log.severe("Trying to inject JNoSQL database but " + DatabaseLogic.class.getName() + "annotation was not provided");
+			throw new RuntimeException(DatabaseTemplate.class.getName() + " has to be annotated with " + DatabaseLogic.class.getName());
+		}
+
+		databaseType = databaseAnnotation.databaseType();
+
+		try {
+			switch (databaseType) {
+				case GRAPH:
+					databaseClass = Class.forName(graphTemplateClassName);
+					break;
+				case COLUMN:
+					databaseClass = Class.forName(columnTemplateClassName);
+					break;
+				case DOCUMENT:
+					databaseClass = Class.forName(documentTemplateClassName);
+					break;
+				case KEY_VALUE:
+					databaseClass = Class.forName(keyValueTemplateClassName);
+					break;
+				default:
+					log.severe("Unknown database type: " + databaseType.toString());
+					throw new IllegalArgumentException("Unknown database type");
+			}
+
+		} catch (ClassNotFoundException e) {
+			log.severe("Unable to create desired template class");
+			e.printStackTrace();
+		}
+
+		AnnotatedType<DatabaseTemplate> annotatedType = bm.createAnnotatedType(DatabaseTemplate.class);
+		databaseTemplate = bm.createBean(bm.createBeanAttributes(annotatedType), DatabaseTemplate.class,
+				bm.getInjectionTargetFactory(annotatedType));
+
+		Set<Bean<?>> allBeans = bm.getBeans(Object.class, new AnnotationLiteral<Any>() {
+		});
+		allBeans.forEach(System.err::println);
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T insert(T entity) {
+		String methodName = null;
+
+		switch (databaseType) {
+			case KEY_VALUE:
+				methodName = "put";
+				break;
+			case DOCUMENT:
+			case COLUMN:
+			case GRAPH:
+				methodName = "insert";
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown database type");
+		}
+
+		try {
+			System.err.println(databaseTemplate.toString());
+			Method insertMethod = databaseTemplate.getClass().getMethod(methodName);
+			return ((T) insertMethod.invoke(databaseTemplate));
+		} catch (NoSuchMethodException e) {
+			log.log(Level.SEVERE, "This method doesn't exist for database type: " + databaseType.toString());
+		} catch (IllegalAccessException e) {
+			log.log(Level.SEVERE, "Illegal access", e);
+		} catch (InvocationTargetException e) {
+			log.log(Level.SEVERE, "Constructor or invoked method threw an exception", e);
+		}
+		throw new RuntimeException("Error inserting entity");
+	}
+
+
+}
---
+++ common/src/main/java/com/kumuluz/ee/jnosql/common/DatabaseType.java
@@ -0,0 +1,5 @@
+package com.kumuluz.ee.jnosql.common;
+
+public enum DatabaseType {
+	COLUMN, GRAPH, DOCUMENT, KEY_VALUE
+}
