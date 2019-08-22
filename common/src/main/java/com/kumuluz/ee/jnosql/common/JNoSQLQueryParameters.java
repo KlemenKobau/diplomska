4e4400e6a3914558cb35a0e064b903433ae4d86d
Klemen Kobau <klemen.kobau@gmail.com>
2019-08-22 15:55:03 +0200

    JNoSQL parameters

---
+++ common/src/main/java/com/kumuluz/ee/jnosql/common/JNoSQLQueryParameters.java
@@ -0,0 +1,45 @@
+package com.kumuluz.ee.jnosql.common;
+
+import com.kumuluz.ee.rest.beans.QueryOrder;
+import com.kumuluz.ee.rest.beans.QueryParameters;
+import com.kumuluz.ee.rest.enums.OrderDirection;
+import org.jnosql.artemis.Pagination;
+import org.jnosql.artemis.Sorts;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JNoSQLQueryParameters extends QueryParameters {
+
+	public Sorts getSorts() {
+		List<QueryOrder> queryOrders = getOrder();
+		Sorts sorts = Sorts.sorts();
+
+		for (QueryOrder queryOrder : queryOrders) {
+			if (queryOrder.getOrder().equals(OrderDirection.ASC)) {
+				sorts.asc(queryOrder.getField());
+			} else if (queryOrder.getOrder().equals(OrderDirection.DESC)) {
+				sorts.desc(queryOrder.getField());
+			} else {
+				throw new IllegalArgumentException("Unknown order type: " + queryOrder.getOrder());
+			}
+		}
+		return sorts;
+	}
+
+	public Optional<Pagination> getPagination() {
+		Long offset = getOffset();
+		Long limit = getLimit();
+
+		if (offset == null || limit == null)
+			return Optional.empty();
+
+		if (offset % limit != 0) {
+			// this method can not be used, since we aren't sure what the initial offset is for
+			return Optional.empty();
+		}
+
+		Pagination pagination = Pagination.page(offset / limit).size(limit);
+		return Optional.of(pagination);
+	}
+}
